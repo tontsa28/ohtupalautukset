02ff5db5202c38ea31dfb0c4d3e2da0cc51e0038
tontsa28 <miika@tontsa.fi>
2025-11-02 19:03:35 +0200

    solve week 1 exercise 16

---
+++ viikko1/nhl-statistics/src/tests/statistics_service_test.py
@@ -0,0 +1,30 @@
+import unittest
+from statistics_service import StatisticsService
+from player import Player
+
+class PlayerReaderStub:
+    def get_players(self):
+        return [
+            Player("Semenko", "EDM", 4, 12),  #  4+12 = 16
+            Player("Lemieux", "PIT", 45, 54), # 45+54 = 99
+            Player("Kurri",   "EDM", 37, 53), # 37+53 = 90
+            Player("Yzerman", "DET", 42, 56), # 42+56 = 98
+            Player("Gretzky", "EDM", 35, 89)  # 35+89 = 124
+        ]
+
+class TestStatisticsService(unittest.TestCase):
+    def setUp(self):
+        # annetaan StatisticsService-luokan oliolle "stub"-luokan olio
+        self.stats = StatisticsService(
+            PlayerReaderStub() # type: ignore
+        )
+
+    def test_search(self):
+        self.assertEqual(str(self.stats.search("Kurri")), str(Player("Kurri", "EDM", 37, 53)))
+        self.assertEqual(self.stats.search("Selanne"), None)
+
+    def test_team(self):
+        self.assertEqual(len(self.stats.team("EDM")), 3)
+
+    def test_top(self):
+        self.assertEqual(len(self.stats.top(3)), 4)
