ac2fa7d2b96ca058a2eeee68dcb453b8a54d29c6
tontsa28 <miika@tontsa.fi>
2025-11-02 15:39:52 +0200

    solve week 1 exercise 14

---
+++ viikko1/riippuvuuksien-injektointi/src/tests/laskin_test.py
@@ -0,0 +1,31 @@
+import unittest
+from laskin import Laskin
+
+
+class StubIO:
+    def __init__(self, inputs):
+        self.inputs = inputs
+        self.outputs = []
+
+    def lue(self, teksti):
+        return self.inputs.pop(0)
+
+    def kirjoita(self, teksti):
+        self.outputs.append(teksti)
+
+
+class TestLaskin(unittest.TestCase):
+    def test_yksi_summa_oikein(self):
+        io = StubIO(["1", "3", "-9999"])
+        laskin = Laskin(io)
+        laskin.suorita()
+
+        self.assertEqual(io.outputs[0], "Summa: 4")
+
+    def test_kaksi_summaa_oikein(self):
+        io = StubIO(["8", "4", "-5", "5", "-9999"])
+        laskin = Laskin(io)
+        laskin.suorita()
+
+        self.assertEqual(io.outputs[0], "Summa: 12")
+        self.assertEqual(io.outputs[1], "Summa: 0")
