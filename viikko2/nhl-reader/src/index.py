2a468010e4968379880f4eee02a9e548c70fae9c
tontsa28 <miika@tontsa.fi>
2025-11-07 18:07:20 +0200

    solve week 2 exercise 3

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -5,20 +5,21 @@ def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2024-25/players"
     response = requests.get(url).json()
 
-    print("JSON-muotoinen vastaus:")
-    print(response)
-
     players = []
 
     for player_dict in response:
         player = Player(player_dict)
-        players.append(player)
+        if player.nationality == "FIN":
+            players.append(player)
 
-    print("Oliot:")
+    def sort_by_points(player):
+        return player.points
+
+    print("Players from FIN\n")
+    players.sort(key=sort_by_points, reverse=True)
 
     for player in players:
-        if player.nationality == 'FIN':
-            print(player)
+        print(player)
 
 
 if __name__ == "__main__":
--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -2,6 +2,12 @@ class Player:
     def __init__(self, dict):
         self.name = dict['name']
         self.nationality = dict['nationality']
+        self.assists = int(dict['assists'])
+        self.goals = int(dict['goals'])
+        self.points = self.goals + self.assists
+        self.team = dict['team']
+        self.games = int(dict['games'])
+        self.id = int(dict['id'])
     
     def __str__(self):
-        return self.name
+        return f"{self.name:22}{self.team:16}{self.goals:2} + {self.assists:2} = {self.points}"
