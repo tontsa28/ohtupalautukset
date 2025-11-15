d3ef8d65c8c9d0de1693603efae5add849c887ad
tontsa28 <miika@tontsa.fi>
2025-11-15 20:09:00 +0200

    solve week 3 exercise 7

--- viikko3/login/src/services/user_service.py
+++ viikko3/login/src/services/user_service.py
@@ -41,6 +41,16 @@ class UserService:
             raise UserInputError("Username and password are required")
 
         # toteuta loput tarkastukset tänne ja nosta virhe virhetilanteissa
+        if len(username) < 3:
+            raise UserInputError("Username must be at least 3 characters long")
+        if len(password) < 8:
+            raise UserInputError("Password must be at least 8 characters long")
+        if self._user_repository.find_by_username(username) is not None:
+            raise UserInputError("Username already taken")
+        if password.isalpha():
+            raise UserInputError("Password must contain at least 1 non-letter character")
+        if password != password_confirmation:
+            raise UserInputError("Given passwords did not match")
 
 
 user_service = UserService()
